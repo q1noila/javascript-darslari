cc1bda384fad6503ea17b238a702f92e7c1e4fbe
nolia1206_23 <noila122009@gmail.com>
2025-04-04 21:56:43 +0300

    java

---
+++ 11-dars.js
@@ -0,0 +1,84 @@
+// //Masala-1
+// const greeting=(name)=>{
+//     console.log('Salom',name)
+// }
+// greeting('Ali')
+
+// //Masala-2
+// const oddOrEven=(num)=>{
+//     if (num%2==0){
+//         console.log("Juft")
+//     }
+//     else{
+//         console.log('Toq')
+//     }
+// }
+// oddOrEven(4)
+
+// //Masala-3
+// const sumOfNumbers=function(a,b){
+//     return a+b
+// }
+// console.log(sumOfNumbers(6,2))
+
+// //Masala-4
+// const askUser=function(question){
+//     return question
+// }
+// prompt('Ismingiz nima?')
+
+// //Masala-5
+// let summa=1
+// const factorial=(n)=>{
+//     for(i=1; i<=n; i++){
+//         summa*=i
+//     }
+// }
+// factorial(5)
+// console.log(summa)
+
+
+// //Masala-6
+// const number=(a)=>{
+//     for(i=1;i<=a;i++) {
+//         if(i%3==0 && i%5==0){
+//             console.log(i,'FizzBuzz')
+//         }
+//         else if(i%3==0){
+//             console.log(i,'Fizz')
+//         }
+//         else if(i%5==0){
+//             console.log(i,'Buzz')
+//         }
+//         else{
+//             console.log(i)
+//         } 
+//     }     
+// }
+// number(20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(a%3==0 && a%5==0){
+//     console.log('FizzBuzz')
+// }
+// else if(a%3==0){
+//     console.log('Fizz')
+// }
+// else if(a%5==0){
+//     console.log('Buzz')
+// }
+// else{
+//     console.log(a)
+// }
