227374cc3fb7fe74d875803c852e2d9698c62a99
nolia1206_23 <noila122009@gmail.com>
2025-04-10 15:42:21 +0300

    array

--- 12-dars.js
+++ 12-dars.js
@@ -1,2 +1,18 @@
-const arr=['olma','banan','nok',12]
-console.log(arr[2])
+// const arr=['olma','banan','nok',12]
+// console.log(arr[1])
+
+// let arr1= new Array(1,2,3,4,5)
+// console.log(arr1[2])
+
+// const arr=['olma','banan','nok',12]
+// console.log(arr[arr.length-2])
+
+let numbers=[1,2,3,4]
+numbers.push(5) //oxiriga element qo`shadi
+numbers.unshift(0)//boshiga elementni qo`shadi
+numbers[1]=12 //indexni ichida yozilgan sonni yangilaydi
+numbers.pop()//oxiridagi elementni o`chiradi
+numbers.shift()//boshidagi elementni o`chiradi
+console.log(numbers)
+
+
