c6a20a37c021e1560e554190e7a4508c11a155eb
nolia1206_23 <noila122009@gmail.com>
2025-04-17 15:35:42 +0300

    masala

---
+++ masala/masala.js
@@ -0,0 +1,98 @@
+// // //1-Masala
+// let numbers=[2,5,7,10]
+// let result=0
+// for(item of numbers){
+//     result+=item
+// }
+// console.log(result)
+
+// //2-masala
+// let nums = [3, 9, 1, 14, 7];
+// let katta=0
+// for(i of nums){
+//     if(katta<i) katta=i
+// }
+// console.log(katta)
+
+
+// //3-masala
+// let numbers = [1, 2, 3, 4, 5, 6];
+// let result=numbers.filter((element)=> element%2==0)
+// console.log(result)
+
+// //4-masala
+// let arr = [2, 3, 4];
+// let kvadrat=arr.map((i)=> i*i)
+// console.log(kvadrat)
+
+
+// //5-masala
+// let fruits = ['olma', 'banan', 'shaftoli'];
+// console.log(fruits.includes('olma'))
+
+// //6-masala
+// let user = { name: 'Ali', age: 20 };
+// let {name,age}=user
+// console.log(name,age)
+
+// //7-masala
+// let student = { name: 'Hasan' };
+// student.email='email@gmail.com'
+// console.log(student)
+
+// //8-masala
+// let person = { name: 'Olim', age: 25, city: 'Toshkent' };
+// for(i in person){
+//     console.log(person[i])
+// }
+
+// //9-masala
+// let users = [
+//     { name: 'Ali', age: 17 },
+//     { name: 'Vali', age: 21 },
+//     { name: 'Sami', age: 19 }
+//   ];
+// // for (i of users){
+// //     if(i.age>18){
+// //         console.log(i.name)
+// //     }
+// // }
+// users=users.fillter((i)=>i>17)
+// console.log(users)
+
+// //10-masala
+// let countries = ['Uzbekistan', 'Russia', 'USA', 'Germany'];
+// console.log(countries.sort((a,b)=>a.localeCompare(b)))
+
+// //11-masala
+// let car = { brand: 'BMW', color: 'black', year: 2020 };
+// let arr=[]
+// for(i in car){
+//     arr.push(i)
+// }
+// console.log(arr.length)
+
+
+
+// //12-masala
+// let people = [
+//     { name: 'Aziz' },
+//     { name: 'Dilshod' },
+//     { name: 'Jamshid' }
+//   ];
+// for(i of people){
+//     console.log(i.name)
+// }
+
+// //13-masala
+// let a = [1, 2]; let b = [3, 4];
+// console.log(a.concat(b))
+
+// //14-masala
+// let arr = [1, 2, 3, 4, 5];
+// arr.splice(2,1)
+// console.log(arr)
+
+// //15-masala
+// let arr = [10, 20, 30, 40, 50];
+// console.log(arr.slice(0,3))
