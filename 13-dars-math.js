772fabe6a8ce441bc54202b395907ae5fdd986d5
nolia1206_23 <noila122009@gmail.com>
2025-04-22 15:31:44 +0500

    math,string

--- 13-dars-math.js
+++ 13-dars-math.js
@@ -9,10 +9,36 @@
 // console.log(Math.trunc(23.324234)) //trunc kasr qismini tashlab chiqarib beradi
 
 // console.log(Math.random())//0 dan 1 gacha bo`lgan tasodifiy sonni chiqaradi
+// console.log(Math.random()*100) //tasodifiy sonlarni 100 ko`paytirib chiqaradi
 
 
- generate=(son)=>{ //0 dan generate()ni ichidagi bo`lgan tasodifiy sonlar
-    console.log(Math.trunc(Math.random()*son))
-}
-generate(10)
+//  generate=(son)=>{ //0 dan generate()ni ichidagi bo`lgan tasodifiy sonlar
+//     console.log(Math.trunc(Math.random()*son))
+// }
+// generate(10)
+
+// =============================================================
+// console.log(Math.min(1,2,3,4,5,6,7,8,9))
+// console.log(Math.max(1,2,3,4,5,6,7,8,9))
+
+// let sonlar=[1,2,3,4,5,6,7,8,9]
+// console.log(...sonlar)//sonlarni o`zini tekshira olmaydi shunga arraydan chiqarib tashlaymiz
+
+// console.log(Math.pow(2,4))//pow(1,2)- 1-son, 2-1sonni darajasi
+
+// console.log(Math.sqrt(9)) //sqrt()-kvadrat ildizini chiqarish
+// console.log(Math.cbrt(27))//cbrt()-kub ildizini chiqarish
+
+// console.log(Math.sign(-0))//sign()-son yo musbat yo manfiy yo 0 ligini tekshiradi
+
+// console.log(Object.is(-0,+0))//"===" dan ham chuqurroq tekshirish  //false
+
+// console.log(NaN===NaN)//oddiy tekshirganda false qaytaradi
+// console.log(Object.is(NaN,NaN)) //true
+console.log(12.34.toFixed(1)) //sondagi butun qismdan keyin nechta kasr qismi kerakligini belgila
+
+
+
+
+
 
---
+++ 14-dars.js
@@ -0,0 +1,43 @@
+//String method
+// let matn='Men tarraqqiyotda o`qiyman'
+// console.log(matn.toUpperCase())//Hamma harfni katta qiladi
+// console.log(matn.toLowerCase())//Hamma harfni kichik qiladi
+// console.log(matn.indexOf('a'))//shu harfni nechanchi indexda ekanligini ko`rsatadi
+// console.log(matn.includes('Men'))//matn ichidan qidirish
+
+// let users=['Olimjanova','Olijanov','Sadikov','Mamadjanova']
+// users=users.filter((i)=> i.endsWith('va'))//va bilan tugagan so`zlarni topadi
+// console.log(users)
+// users=users.filter((i)=> !i.endsWith('va'))//va bilan tugamagan so`zlarni topadi
+
+// let number='1234'
+// console.log(number.startsWith('12'))
+
+// console.log('hello'.padEnd(8,"-"))//padEnd(l,i) l-so`zning lengthi, i-agar so`zning lenghtdan kam bo`lsa oxiriga nima qo`yish kerakligi
+// console.log('hello'.padStart(8,"-"))//padEnd(l,i) l-so`zning lengthi, i-agar so`zning lenghtdan kam bo`lsa boshiga nima qo`yish kerakligi
+
+// console.log('JavaScript'.slice(0,4)) //slice(start,end) qayerdan qayergacha chiqarish
+
+// let soz=' salom '
+// soz=soz.trimStart()//boshidagi bo`sh joyni olib tashlaydi
+// soz=soz.trimEnd()//oxiridagi bo`sh joyni olib tashlaydi
+// soz=soz.trim()//boshidagi va oxiridagi bo`sh joylani olib tashlaydi
+// console.log(soz.split("")) //so`zlarni alohida element qilib arrayga o`zgartiradi
+
+
+// console.log('nima'.repeat(100))  //repeat(n) so`zni n marta takrorlash
+// console.log("salom".concat(" do'stim", 'nima gapla'))  //sozlarni qo`shish uchun
+// ↕ikkalasi bir xil 
+// console.log('salom' + '' + 'do`stim')
+
+
+// console.log(eval('1+2+3+4+5+6+7')) //matn bo`lsa ham numberga o`zgartirib ishlab beradi 
+// // ↕ikkalasi bir xil 
+// let sonlar = [1,2,3,4,5,6,7]
+// sonlar = sonlar.join('+') 
+// console.log(eval(sonlar))
+
+
+
+
+
