29db89346b0bbeee8344298dd93f63080a84f2cf
nolia1206_23 <noila122009@gmail.com>
2025-05-20 14:09:12 +0500

    java

--- 21-dars/app.js
+++ 21-dars/app.js
@@ -1,34 +1,70 @@
-// const title=document.getElementById("title");
-const toggle = document.getElementById("toggle");
-const modal = document.querySelector(".modal");
-const close = document.getElementById("close");
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const parol = document.getElementById("parol");
-const submit = document.getElementById("submit");
+// // const title=document.getElementById("title");
+// const toggle = document.getElementById("toggle");
+// const modal = document.querySelector(".modal");
+// const close = document.getElementById("close");
+// const form = document.getElementById("form");
+// const username = document.getElementById("username");
+// const parol = document.getElementById("parol");
+// const submit = document.getElementById("submit");
 
-// title.oncontextmenu=()=>{ //salom deb yozgan narsa ustiga sichqonchani o`ng tarafini bosganda chiqadigan narsa 
-//     alert('menu')
+// // title.oncontextmenu=()=>{ //salom deb yozgan narsa ustiga sichqonchani o`ng tarafini bosganda chiqadigan narsa 
+// //     alert('menu')
+// // }
+
+// // modal.classList.add('openModal')
+// toggle.onclick = () => {
+//     modal.classList.remove('openModal')
+// }
+// close.onclick = () => {
+//     modal.classList.add('openModal')
+// }
+// modal.onclick = (e) => {
+//     if (e.target.className == "modal") {
+//         modal.classList.add('openModal')
+//     }
 // }
 
-// modal.classList.add('openModal')
-toggle.onclick = () => {
-    modal.classList.remove('openModal')
-}
-close.onclick = () => {
-    modal.classList.add('openModal')
-}
-modal.onclick = (e) => {
-    if (e.target.className == "modal") {
-        modal.classList.add('openModal')
-    }
-}
+// submit.onclick = (e) => {
+//     e.preventDefault();
+//     let ism = username.value;
+//     let pas = parol.value;
+//     console.log(ism, pas);
+//     username.value=''
+//     parol.value=''
+// }
 
-submit.onclick = (e) => {
-    e.preventDefault();
-    let ism = username.value;
-    let pas = parol.value;
-    console.log(ism, pas);
-    username.value=''
-    parol.value=''
+
+// =====================================================
+const btn=document.querySelector('.btn')
+const left=document.getElementById('left')
+const topp=document.getElementById('top')
+const right=document.getElementById('right')
+const closed=document.getElementById('closed')
+const nots=document.querySelector('.nots')
+
+nots.classList.add('openModal')
+left.onclick=()=>{
+    nots.classList.remove('openModal')
+    setTimeout(()=>{
+        nots.classList.add('openModal')
+    },3000)
+}
+topp.onclick=()=>{
+    nots.classList.remove('openModal');
+    nots.classList.add('topp')
+    setTimeout(()=>{
+        nots.classList.add('openModal')
+        nots.classList.remove('topp')
+    },3000)
+}
+right.onclick=()=>{
+    nots.classList.remove('openModal');
+    nots.classList.add('right')
+    setTimeout(()=>{
+        nots.classList.add('openModal')
+        nots.classList.remove('right')
+    },3000)
+}
+closed.onclick = () => {
+    nots.classList.add('openModal')
 }
